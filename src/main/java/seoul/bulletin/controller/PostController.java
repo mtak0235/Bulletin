c914c9086ac6c88d5caa72c6def931719f5ece71
mtak <jesustark0235@gmail.com>
2022-02-23 03:38:55 +0900

    등록 삭제 뷰 구현할 것. html파일에 js 안 얹어지는 이유 찾기

--- src/main/java/seoul/bulletin/controller/PostController.java
+++ src/main/java/seoul/bulletin/controller/PostController.java
@@ -1,6 +1,7 @@
 package seoul.bulletin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import seoul.bulletin.service.PostsService;
 import seoul.bulletin.web.PostsResponseDto;
@@ -26,5 +27,16 @@ public class PostController {
     @GetMapping("/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts_update";
+    }
+    @DeleteMapping("/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
--- src/main/java/seoul/bulletin/service/PostsService.java
+++ src/main/java/seoul/bulletin/service/PostsService.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import seoul.bulletin.domain.Posts;
 import seoul.bulletin.domain.PostsRepository;
+import seoul.bulletin.web.PostsListResponseDto;
 import seoul.bulletin.web.PostsResponseDto;
 import seoul.bulletin.web.PostsSaveRequestDto;
 import seoul.bulletin.web.PostsUpdateRequestDto;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +34,20 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("그런 게시글 없다. id" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("그런 게시글 없다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
